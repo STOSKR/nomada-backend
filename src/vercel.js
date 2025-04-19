c20bc3806b5d9f9e2d4e2e2ee59940bccf50e46f
Shiyi <scheng1@upv.edu.es>
2025-04-19 19:17:15 +0200

    Corrección createApp is not a function

--- src/vercel.js
+++ src/vercel.js
@@ -1,27 +1,7 @@
 'use strict';
 
-// Importar la función createApp desde server.js
-const { createApp } = require('./server');
-
-// Para entorno serverless
-let cachedApp;
+// Importar la función createApp y el handler desde server.js
+const { handler } = require('./server');
 
 // Exportar para Vercel
-module.exports = async (req, res) => {
-    try {
-        if (!cachedApp) {
-            cachedApp = createApp();
-            await cachedApp.ready();
-        }
-
-        cachedApp.server.emit('request', req, res);
-    } catch (err) {
-        res.statusCode = 500;
-        res.end(JSON.stringify({
-            success: false,
-            message: 'Error interno del servidor',
-            error: err.message,
-            stack: process.env.NODE_ENV === 'development' ? err.stack : undefined
-        }));
-    }
-}; 
+module.exports = handler; 
